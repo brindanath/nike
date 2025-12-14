0e962f052be01ebbb51eea7785e0d3324c00e351
brindanath <brindanath@gmail.com>
2025-12-14 02:07:14 -0800

    working nike jar

--- src/main/java/com/brindys/deTools/DeToolsApplication.java
+++ src/main/java/com/brindys/deTools/DeToolsApplication.java
@@ -1,4 +1,4 @@
-package com.brindys.deTools.visitTypeMapper;
+package com.brindys.deTools;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -8,23 +8,31 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
+    "com.brindys.deTools",  // Add this to scan the base package
     "com.brindys.deTools.visitTypeMapper",
     "com.brindys.deTools.configValidator"
 })
-public class VisitTypeToolsApplication implements CommandLineRunner {
+public class DeToolsApplication implements CommandLineRunner {
 
   @Value("${server.port}")
   private String serverPort;
 
   public static void main(String[] args) {
-    SpringApplication.run(VisitTypeToolsApplication.class, args);
+    SpringApplication.run(DeToolsApplication.class, args);
   }
 
   @Override
   public void run(String... args) {
     System.out.println("========================================");
-    System.out.println("Visit Type Tools Server Running!");
+    System.out.println("DE Tools Server Running!");
     System.out.println("Access at: http://localhost:" + serverPort);
+    System.out.println("========================================");
+    System.out.println("Available Tools:");
+    System.out.println("  - PSL Release Notes Generator");
+    System.out.println("  - Visit Type Mapper");
+    System.out.println("  - Config Formatter");
+    System.out.println("  - Config Checker");
+    System.out.println("  - PSL Feature Lineage Mapper");
     System.out.println("========================================");
   }
 }
---
+++ src/main/java/com/brindys/deTools/PSLLineageController.java
@@ -0,0 +1,41 @@
+package com.brindys.deTools;
+
+
+import com.brindys.deTools.pslFeatureMapper.PSLFeatureMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+@RestController
+@RequestMapping("/api")
+public class PSLLineageController {
+
+  @PostMapping(value = "/psl-lineage", consumes = "text/plain", produces = "text/plain")
+  public ResponseEntity<String> generateLineage(@RequestBody String pslConfig) {
+    try {
+      PSLFeatureMapper mapper = new PSLFeatureMapper();
+      mapper.parse(pslConfig);
+
+      // Capture the output
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      PrintStream ps = new PrintStream(baos);
+      PrintStream old = System.out;
+      System.setOut(ps);
+
+      mapper.printLineage();
+
+      System.out.flush();
+      System.setOut(old);
+
+      String lineageOutput = baos.toString();
+      return ResponseEntity.ok(lineageOutput);
+
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("Error generating lineage: " + e.getMessage());
+    }
+  }
+}
